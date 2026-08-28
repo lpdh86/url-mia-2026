9c8ed759333335707982e46f2fa2b27f113e9c28
lpdh86 <LPDELEONH@correo.url.edu.gt>
2026-08-27 21:22:07 -0600

    Add files via upload

---
+++ semana8/url-mia-2026/Laboratorio_2_CSV_JSON/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        List<Estudiante> estudiantes = new List<Estudiante>();
+
+        string[] lineas = File.ReadAllLines("estudiantes.csv");
+
+        for (int i = 1; i < lineas.Length; i++)
+        {
+            string[] datos = lineas[i].Split(',');
+
+            Estudiante estudiante = new Estudiante
+            {
+                Id = int.Parse(datos[0]),
+                Nombre = datos[1],
+                Carrera = datos[2],
+            };
+
+            estudiantes.Add(estudiante);
+        }
+
+        foreach (Estudiante e in estudiantes)
+        {
+            Console.WriteLine($"{e.Id} - {e.Nombre} - {e.Carrera}");
+        }
+
+        string json = JsonSerializer.Serialize(estudiantes, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+
+        File.WriteAllText("estudiantes.json", json);
+
+        Console.WriteLine("Archivo estudiantes.json creado correctamente.");
+    }
+}
---
+++ semana8/url-mia-2026/README.md
@@ -0,0 +1,10 @@
+\# Laboratorio No. 1 - Git
+
+Nombre: Luis de leon
+
+Carné: 1227025
+
+Curso: Métodos e Implementación de Archivos
+
+
+
